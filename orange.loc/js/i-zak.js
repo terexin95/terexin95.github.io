539f4e148b6d8e9ebf5a646172eefd1d42eae5d5
terexin95 <39961660+terexin95@users.noreply.github.com>
2019-12-15 17:30:39 +0300

    1

---
+++ orange.loc/js/i-zak.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function(){
+    $(document).on('click','.i-zak',function () {
+        var s = $(this);
+        if(s.parent().get(0).tagName != 'LABEL' && s.parent().parent().get(0).tagName != 'LABEL' && s.parent().parent().parent().get(0).tagName != 'LABEL'){
+            s.prev().trigger('click');
+        }
+    });
+    $(document).on('change','input[type=checkbox],input[type=radio]',function(){
+        var i = $(this);
+        var s = i.next();
+        // console.log(i.attr('type')== 'checkbox');
+        if(i.attr('type')== 'checkbox'){
+            if(!i.is(':checked')){
+                s.removeClass('checked');
+                i.removeAttr('checked');
+            }else{
+                s.addClass('checked');
+                i.attr('checked',1);
+            }
+        }else if(i.attr('type')== 'radio'){
+            $.each($('input[name='+i.attr('name')+']'), function(){
+                $(this).next().removeClass('checked');
+            });
+            i.next().addClass('checked');
+        }
+    });
+    $(document).on('change','input[name=type_flat]',function(){
+        var inp = $(this).val();
+        var img = $('#photo_demonstration_1');
+        if(inp=='Новостройка'){
+            img.attr('src','images/photo_1_for_question_one.png');
+        }else{
+            img.attr('src','images/photo_2_for_question_one.png');
+        }
+    });
+});
---
+++ orange.loc/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "4994043615@mail.ru";
+$sitename = "Мандарины";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
