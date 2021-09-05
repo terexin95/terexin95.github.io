40c6593d09fe94f21cafb83bbdd9ac91549f6d4e
terexin95 <39961660+terexin95@users.noreply.github.com>
2021-09-05 03:39:34 +0300

    2

--- sellauto.loc/js/main.js
+++ sellauto.loc/js/main.js
@@ -24,3 +24,32 @@ document.querySelectorAll('.tabs').forEach(tab =>{
   const tabs = new Tabs(tab);
   tabs.init();
 })
+
+
+if (document.querySelector('.single__images img')) {
+  var singleImg = document.querySelectorAll('.single__images img');
+  for (var i = 0; i < singleImg.length; i++) {
+    singleImg[i].addEventListener('click', function(){
+      document.querySelector('.modal-carusel').classList.add('modal-carusel--active');
+    });
+  }
+
+  document.querySelector('.single__images-item-overlay').addEventListener('click', function(){
+    document.querySelector('.modal-carusel').classList.add('modal-carusel--active');
+  });
+
+  document.querySelector('.modal-carusel__close').addEventListener('click', function(){
+    document.querySelector('.modal-carusel').classList.remove('modal-carusel--active');
+  });
+}
+
+if (document.getElementsByClassName("accordion__title")) {
+  var acc = document.getElementsByClassName("accordion__title");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.parentElement.classList.toggle("accordion__item--active");
+    });
+  }
+}
