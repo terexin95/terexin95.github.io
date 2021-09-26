80b2ea1fab76f298b26081500a2b74f5278364f3
terexin95 <39961660+terexin95@users.noreply.github.com>
2021-09-26 19:18:04 +0300

    2

--- fly.loc/js/main.js
+++
@@ -1,8 +0,0 @@
-if (document.querySelector('.input-check__input')) {
-	var inputCheckbox = document.querySelectorAll('.input-check__input');
-	for (var i = 0; i < inputCheckbox.length; i++) {
-		inputCheckbox[i].addEventListener('click', function(){
-			this.classList.toggle('input-check__input--active');
-		})
-	}
-}
