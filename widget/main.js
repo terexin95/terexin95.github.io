693cfbff74255349f6873f008e3bdec4a7e9453f
terexin95 <39961660+terexin95@users.noreply.github.com>
2021-10-22 04:02:31 +0300

    4

--- widget/main.js
+++ widget/main.js
@@ -12,7 +12,7 @@ let ddMax = +dd + 5;
 let todayMax = yyyy + '-' + mm + '-' + ddMax;
 
 remarkedWidgetClassic.innerHTML += `
-  <style>input{height:40px;border-radius:4px;-webkit-box-sizing:border-box;box-sizing:border-box;-webkit-box-shadow:0 0 0 0 transparent;box-shadow:0 0 0 0 transparent;border:1px solid #ccc;outline:0;padding:0 15px}input:focus{border-color:#1aaf33}input[type=tel],input[type=text]{width:100%}.overlay{position:fixed;left:0;right:0;top:0;bottom:0;background-color:rgba(0,0,0,.55);z-index:10000;opacity:0;visibility:hidden;-webkit-transition:.55s opacity,.55s visibility;-o-transition:.55s opacity,.55s visibility;transition:.55s opacity,.55s visibility}.remarked-widget{width:700px;position:fixed;background-color:#fff;left:50%;top:50%;-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%);z-index:10001;border-radius:20px;color:#fff;opacity:0;visibility:hidden;-webkit-transition:.55s opacity,.55s visibility;-o-transition:.55s opacity,.55s visibility;transition:.55s opacity,.55s visibility}.remarked-widget-classic__header{padding:20px 30px;background-color:#a50204;border-radius:20px 20px 0 0;display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-ms-flex-align:center;align-items:center}.remarked-widget__close{cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.remarked-widget-classic__body{padding:30px}.remarked-widget__userdata{display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;margin-bottom:20px}.remarked-widget__userdata input{max-width:47%;-webkit-box-flex:0;-ms-flex:0 0 47%;flex:0 0 47%}.remarked-widget-row{display:-webkit-box;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap;margin:0 -20px;margin-bottom:20px}.remarked-widget-column{max-width:50%;-webkit-box-flex:0;-ms-flex:0 0 50%;flex:0 0 50%;padding:0 20px;-webkit-box-sizing:border-box;box-sizing:border-box}.remarked-widget__times{display:-webkit-box;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap}.remarked-widget__time{max-width:13%;-webkit-box-flex:0;-ms-flex:0 0 13%;flex:0 0 13%;margin-right:1.5%;color:#333;text-align:center;background-color:#ededed;height:40px;border-radius:8px;cursor:pointer;margin-bottom:1.5%;line-height:40px}.remarked-widget__time:nth-child(7n){margin-right:0}.remarked-widget__comment{margin-top:15px;margin-bottom:20px}button{height:40px;padding:0 30px;background-color:#1aaf33;border:0;border-radius:4px;-webkit-box-shadow:0 0 0 0 transparent;box-shadow:0 0 0 0 transparent;cursor:pointer;color:#fff;-webkit-transition:all .3s ease;-o-transition:all .3s ease;transition:all .3s ease}button:active,button:focus,button:hover{background-color:#19a330;-webkit-transition:all .3s ease;-o-transition:all .3s ease;transition:all .3s ease}.remarked-widget-none{position:relative;z-index:-1}.remarked-widget-active{position:relative;z-index:1}.remarked-widget-active .overlay,.remarked-widget-active .remarked-widget{opacity:1;visibility:visible}.remarked-widget-title{color:#8a8a8a;margin-bottom:15px;font-weight:500}</style>
+  
 	<div class="overlay"></div>
 	<div class="remarked-widget remarked-widget-classic">
 		<div class="remarked-widget-classic__header">
@@ -28,12 +28,20 @@ remarkedWidgetClassic.innerHTML += `
 				<div class="remarked-widget-title">Ваши данные</div>
 				<div class="remarked-widget-row">
 					<div class="remarked-widget-column">
-						<input type="text" placeholder="Имя" name="userName">
+						<input type="text" placeholder="Имя" id="remarkedUserInput" name="userName">
 					</div>
 					<div class="remarked-widget-column">
-						<input type="tel" placeholder="Телефон" name="userPhone">
+						<input type="tel" class="remarked-phone" id="remarkedPhoneInput" name="userPhone" placeholder="(900) 123-4567">
+					</div>
+					<div class="remarked-widget-column mt-2">
+						<input type="text" placeholder="email@exemple.ru" id="remarkedEmailInput" name="userEmail">
 					</div>
 				</div>
+				<div class="remarked-widget-text">
+					«Пожалуйста, сохраните в телефоне сообщение о подтверждении бронирования от ресторана.» 
+				</div>
+			</div>
+			<div class="remarked-widget-classic__step remarked-widget-classic__step-2">
 				<div class="remarked-widget-row">
 					<div class="remarked-widget-column">
 						<div class="remarked-widget-title">Выберите дату</div>
@@ -41,17 +49,19 @@ remarkedWidgetClassic.innerHTML += `
 					</div>
 					<div class="remarked-widget-column">
 						<div class="remarked-widget-title">Количество гостей</div>
-						<input type="number" value="2" max="10" min="1" name="current">
+						<div class="remarked-quantity">
+						  <input type="number" value="1" readonly>
+						</div>
 					</div>
 				</div>
+				<button id="nextStep3" class="mb-2">Продолжить</button>
+				<div class="remarked-widget-text">
+					«Пожалуйста, сохраните в телефоне сообщение о подтверждении бронирования от ресторана.» 
+				</div>
+			</div>
+			<div class="remarked-widget-classic__step remarked-widget-classic__step-3">
 				<div class="remarked-widget-title">Время</div>
 				<div class="remarked-widget__times">
-					<div class="remarked-widget__time">9:00</div>
-					<div class="remarked-widget__time">9:30</div>
-					<div class="remarked-widget__time">10:00</div>
-					<div class="remarked-widget__time">10:30</div>
-					<div class="remarked-widget__time">11:00</div>
-					<div class="remarked-widget__time">11:30</div>
 					<div class="remarked-widget__time">12:00</div>
 					<div class="remarked-widget__time">12:30</div>
 					<div class="remarked-widget__time">13:00</div>
@@ -59,6 +69,20 @@ remarkedWidgetClassic.innerHTML += `
 					<div class="remarked-widget__time">14:00</div>
 					<div class="remarked-widget__time">14:30</div>
 					<div class="remarked-widget__time">15:00</div>
+					<div class="remarked-widget__time">15:30</div>
+					<div class="remarked-widget__time">16:00</div>
+					<div class="remarked-widget__time">16:30</div>
+					<div class="remarked-widget__time">17:00</div>
+					<div class="remarked-widget__time">17:30</div>
+					<div class="remarked-widget__time">18:00</div>
+					<div class="remarked-widget__time">18:30</div>
+					<div class="remarked-widget__time">19:00</div>
+					<div class="remarked-widget__time">19:30</div>
+					<div class="remarked-widget__time">20:00</div>
+					<div class="remarked-widget__time">20:30</div>
+					<div class="remarked-widget__time">21:00</div>
+					<div class="remarked-widget__time">21:30</div>
+					<div class="remarked-widget__time">22:00</div>
 				</div>
 				<div class="remarked-widget__comment">
 					<input type="text" placeholder="Пожелание к бронированию">
@@ -70,7 +94,7 @@ remarkedWidgetClassic.innerHTML += `
 `;
 
 document.querySelector('body').append(remarkedWidgetClassic);
-
+let remarkedOverlay = document.querySelector('.overlay'); 
 let remarkedClose = document.querySelector('.remarked-widget__close');
 
 remarkedClose.addEventListener('click', function(){
@@ -78,9 +102,12 @@ remarkedClose.addEventListener('click', function(){
 	remarkedWidgetClassic.classList.add('remarked-widget-none');
 });
 
-let remarkedOpenWidget = document.querySelectorAll('.remarked-open-widget');
+remarkedOverlay.addEventListener('click', function(){
+	remarkedWidgetClassic.classList.remove('remarked-widget-active');
+	remarkedWidgetClassic.classList.add('remarked-widget-none');
+});
 
-console.log(remarkedOpenWidget);
+let remarkedOpenWidget = document.querySelectorAll('.remarked-open-widget');
 
 for (var i = 0; i < remarkedOpenWidget.length; i++) {
 	remarkedOpenWidget[i].addEventListener('click', function(){
@@ -88,3 +115,170 @@ for (var i = 0; i < remarkedOpenWidget.length; i++) {
 		remarkedWidgetClassic.classList.remove('remarked-widget-none');
 	});
 }
+
+let remarkedPhone = document.querySelector('.remarked-phone');
+
+remarkedPhone.addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+
+
+//Количество гостей
+
+/*
+*
+* Custom quantity input
+* Element requires following HTML structure
+* <div class="quantity"><input type="number"/></div>
+* 
+*
+*/
+
+function quantityInput(element, options) {
+  const spinner = element;
+
+  const defaultOptions = {
+    min: 1,
+    max: 250,
+    value: 1,
+  };
+
+  options = Object.assign({}, defaultOptions, options);
+
+  const obj = {
+
+    input: spinner.querySelector('input[type="number"]'),
+    init() {
+      this.setup();
+      this.events();
+      return this;
+    },
+    setup() {
+
+      this.input.value = options.value;
+      this.max = options.max;
+      this.min = options.min;
+
+      const qNav = document.createElement('div');
+      const qUp = document.createElement('div');
+      const qDown = document.createElement('div');
+
+      qNav.setAttribute('class', 'remarked-quantity-nav');
+      qUp.setAttribute('class', 'remarked-quantity-button remarked-quantity-button--up');
+      qDown.setAttribute('class', 'remarked-quantity-button remarked-quantity-button--down');
+
+      qUp.innerHTML = '+';
+      qDown.innerHTML = '-';
+      qNav.appendChild(qUp);
+      qNav.appendChild(qDown);
+      spinner.appendChild(qNav);
+
+      this.btnUp = spinner.querySelector('.remarked-quantity-button--up');
+      this.btnDown = spinner.querySelector('.remarked-quantity-button--down');
+    },
+    trigger() {
+      const event = document.createEvent('HTMLEvents');
+      event.initEvent('change', true, false);
+      return event;
+    },
+    events() {
+      this.btnUp.addEventListener('click', () => {
+        const oldValue = parseFloat(this.input.value);
+        let newVal;
+        if (oldValue >= this.max) {
+          newVal = oldValue;
+        } else {
+          newVal = oldValue + 1;
+        }
+        this.input.value = newVal;
+        this.input.dispatchEvent(this.trigger());
+      });
+
+      this.btnDown.addEventListener('click', () => {
+        const oldValue = parseFloat(this.input.value);
+        let newVal;
+        if (oldValue <= this.min) {
+          newVal = oldValue;
+        } else {
+          newVal = oldValue - 1;
+        }
+        this.input.value = newVal;
+        this.input.dispatchEvent(this.trigger());
+      });
+      this.input.addEventListener('change', () => {
+        if (parseInt(this.input.value, 16) < this.min) {
+          this.input.value = this.min;
+        }
+        if (parseInt(this.input.value, 16) > this.max) {
+          this.input.value = this.max;
+        }
+      });
+    },
+  }
+  return obj.init();
+}
+
+const numberInputs = document.querySelectorAll(".remarked-quantity");
+
+if (numberInputs.length > 0) {
+  numberInputs.forEach((el, index) => {
+    quantityInput(el, {
+      min: 1,
+      max: 10,
+      value: 1,
+    });
+  });
+}
+
+let remarkedUserInput = document.querySelector('#remarkedUserInput');
+let remarkedPhoneInput = document.querySelector('#remarkedPhoneInput');
+let remarkedEmailInput = document.querySelector('#remarkedEmailInput');
+
+let remarkedUserInputVal = false;
+let remarkedPhoneInputVal = false;
+let remarkedEmailInputVal = false;
+
+remarkedUserInput.addEventListener('change', function(){
+	if (remarkedUserInput.value !== '') {
+		remarkedUserInputVal = true;
+	} else {
+		remarkedUserInputVal = false;
+	}
+	if(remarkedUserInputVal && remarkedPhoneInputVal && remarkedEmailInputVal) {
+		document.querySelector('.remarked-widget-classic__step-1').classList.add('remarked-widget-classic__step-1--none');
+		document.querySelector('.remarked-widget-classic__step-2').classList.add('remarked-widget-classic__step-2--active');
+	}
+});
+
+remarkedPhoneInput.addEventListener('change', function(){
+	if (remarkedPhoneInput.value !== '' && remarkedPhoneInput.value.length == 14) {
+		remarkedPhoneInputVal = true;
+	} else {
+		remarkedPhoneInputVal = false;
+	}
+	if(remarkedUserInputVal && remarkedPhoneInputVal && remarkedEmailInputVal) {
+		document.querySelector('.remarked-widget-classic__step-1').classList.add('remarked-widget-classic__step-1--none');
+		document.querySelector('.remarked-widget-classic__step-2').classList.add('remarked-widget-classic__step-2--active');
+	}
+});
+
+remarkedEmailInput.addEventListener('change', function(){
+	if (remarkedEmailInput.value !== '') {
+		remarkedEmailInputVal = true;
+	} else {
+		remarkedEmailInputVal = false;
+	}
+	if(remarkedUserInputVal && remarkedPhoneInputVal && remarkedEmailInputVal) {
+		document.querySelector('.remarked-widget-classic__step-1').classList.add('remarked-widget-classic__step-1--none');
+		document.querySelector('.remarked-widget-classic__step-2').classList.add('remarked-widget-classic__step-2--active');
+	}
+});
+
+let nextStep3 = document.querySelector('#nextStep3');
+
+nextStep3.addEventListener('click', function(){
+	document.querySelector('.remarked-widget-classic__step-2').classList.add('remarked-widget-classic__step-2--none');
+	document.querySelector('.remarked-widget-classic__step-2').classList.remove('remarked-widget-classic__step-2--active');
+	document.querySelector('.remarked-widget-classic__step-3').classList.add('remarked-widget-classic__step-3--active');
+});
