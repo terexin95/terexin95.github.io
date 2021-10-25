ae07c36b8ff9ec9cae25b9b3e3535120c4fe0aa9
terexin95 <39961660+terexin95@users.noreply.github.com>
2021-10-25 15:15:20 +0300

    7

--- widget/main.js
+++ widget/main.js
@@ -285,10 +285,15 @@ const remarkedReqUrl = "https://app.remarked.ru/api/v1/ApiReservesWidget";
 
 const remarkedXHR = new XMLHttpRequest();
 
-remarkedXHR.open('POST',{
+const remarkedBody = {
 	method: 'GetToken',
 	request_id: new Date().getTime()
-  },remarkedReqUrl);
+}
+
+let remarkedBodyJSON = JSON.stringify(remarkedBody);
+
+
+remarkedXHR.open('POST', remarkedBodyJSON, remarkedReqUrl);
 
 remarkedXHR.onload = function(){
 	console.log(remarkedXHR.response); 
