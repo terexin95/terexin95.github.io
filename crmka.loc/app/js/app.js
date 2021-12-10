3b7cecf2770f46b5d8b21df86360cd0b83af2898
terexin95 <39961660+terexin95@users.noreply.github.com>
2021-12-10 10:04:55 +0300

    2

--- crmka.loc/app/js/app.js
+++ crmka.loc/app/js/app.js
@@ -30,4 +30,26 @@ document.addEventListener('DOMContentLoaded', () => {
 		  tabs.init();
 		})
 	}
+
+	if (document.querySelector('.fensys__mobile-option')) {
+		document.querySelector('.fensys__mobile-option').addEventListener('click', function(){
+			document.querySelector('.fensys__header .tabs__button-group').classList.toggle('tabs__button-group--block')
+		});
+		let button = document.querySelectorAll('.fensys__header .tabs__button-group .tabs__toggle');
+		for (var i = 0; i < button.length; i++) {
+			button[i].addEventListener('click', function(){
+			  let text = this.getAttribute('data-text');
+			  document.querySelector('.fensys__mobile-option span').textContent = text;
+			  document.querySelector('.fensys__header .tabs__button-group').classList.toggle('tabs__button-group--block');
+			});
+		}
+	}
+	if (document.querySelector('.fensys__humburger')) {
+		document.querySelector('.fensys__humburger').addEventListener('click', function(){
+			document.querySelector('.fensys__sidebar').classList.toggle('fensys__sidebar--block');
+		});
+		document.querySelector('.fensys__sidebar-close').addEventListener('click', function(){
+			document.querySelector('.fensys__sidebar').classList.remove('fensys__sidebar--block');
+		});
+	}
 })
